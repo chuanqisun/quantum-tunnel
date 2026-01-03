196cc0ee06eff4ed13c7566c010bc94e12bc43c7
Chuanqi Sun <chuanqi.sun.dev@gmail.com>
2026-01-03 18:50:05 -0500

    update deploy

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/parallax-hud/",
+  base: "/quantum-tunnel/",
   build: {
     rollupOptions: {
       input: {
